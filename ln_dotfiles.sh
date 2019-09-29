338e0f4ab333de63d19682f241167934a6c2d1a5
Masatoshi Nishiguchi <nishiguchi.masa@gmail.com>
2019-09-29 17:58:43 -0400

    Add .eslintrc.json

--- ln_dotfiles.sh
+++ ln_dotfiles.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
 ln -sf ~/dotfiles/.editorconfig ~/.editorconfig
+ln -sf ~/dotfiles/.eslintrc.json ~/.eslintrc.json
 ln -sf ~/dotfiles/.irbrc ~/.irbrc
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
