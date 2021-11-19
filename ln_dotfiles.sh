5b6491a99ba9a330f5b89486dce8d10ec260717f
Masatoshi N <nishiguchi.masa@gmail.com>
2021-11-19 12:16:22 -0500

    Add .prettierrc

--- ln_dotfiles.sh
+++ ln_dotfiles.sh
@@ -4,6 +4,7 @@ ln -sf ~/dotfiles/.editorconfig ~/.editorconfig
 ln -sf ~/dotfiles/.eslintrc.json ~/.eslintrc.json
 ln -sf ~/dotfiles/.iex.exs ~/.iex.exs
 ln -sf ~/dotfiles/.irbrc ~/.irbrc
+ln -sf ~/dotfiles/.prettierrc ~/.config/.prettierrc
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/starship.toml ~/.config/starship.toml
