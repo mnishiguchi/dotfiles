b7f371a0afb288a92d8a6f809305839e62850d16
Masatoshi N <nishiguchi.masa@gmail.com>
2022-05-07 16:12:52 -0400

    Remove .irbrc

--- ln_dotfiles.sh
+++ ln_dotfiles.sh
@@ -3,7 +3,6 @@ ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
 ln -sf ~/dotfiles/.editorconfig ~/.editorconfig
 ln -sf ~/dotfiles/.eslintrc.json ~/.eslintrc.json
 ln -sf ~/dotfiles/.iex.exs ~/.iex.exs
-ln -sf ~/dotfiles/.irbrc ~/.irbrc
 ln -sf ~/dotfiles/.prettierrc ~/.config/.prettierrc
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
