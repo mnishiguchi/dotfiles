c040c85620c3da39e364e474b9fb44d045e84383
Masatoshi N <nishiguchi.masa@gmail.com>
2022-05-07 16:17:06 -0400

    Remove .bash_profile

--- ln_dotfiles.sh
+++ ln_dotfiles.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-ln -sf ~/dotfiles/.bash_profile ~/.bash_profile
 ln -sf ~/dotfiles/.editorconfig ~/.editorconfig
 ln -sf ~/dotfiles/.eslintrc.json ~/.eslintrc.json
 ln -sf ~/dotfiles/.iex.exs ~/.iex.exs
