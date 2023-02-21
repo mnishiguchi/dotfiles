4c4ad2d6883fcc9f22998ae05b1b4a781ffa0f22
Masatoshi N <nishiguchi.masa@gmail.com>
2023-02-21 09:23:34 -0500

    Make harpoon wider

--- nvim/after/plugin/harpoon.lua
+++ nvim/after/plugin/harpoon.lua
@@ -1,4 +1,10 @@
 -- https://github.com/ThePrimeagen/harpoon
+require('harpoon').setup({
+  menu = {
+    width = vim.api.nvim_win_get_width(0) - 4,
+  }
+})
+
 local mark = require('harpoon.mark')
 local ui = require('harpoon.ui')
 
