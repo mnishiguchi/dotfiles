22f9b0c5989ad3a4eaac1dd2dc1d59259ec2d623
Masatoshi N <nishiguchi.masa@gmail.com>
2023-03-04 12:07:00 -0500

    fix: handle error on lsp-zero setup
    * error seems related to friendly-snippets
    * error happened occasionally when staring nvim
    * error probably benign

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -2,4 +2,8 @@ local lsp = require('lsp-zero')
 
 lsp.preset('recommended')
 
-lsp.setup()
+if pcall(lsp.setup) then
+  print('lsp-zero configured successfully')
+else
+  print('error configuring lsp-zero')
+end
