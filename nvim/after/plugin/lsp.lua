4fecc9b0d8772722473f94a56721d3bb8f72468b
Masatoshi N <nishiguchi.masa@gmail.com>
2023-03-08 06:31:25 -0500

    ignore lsp-zero error

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -2,8 +2,4 @@ local lsp = require('lsp-zero')
 
 lsp.preset('recommended')
 
-if pcall(lsp.setup) then
-  print('lsp-zero configured successfully')
-else
-  print('error configuring lsp-zero')
-end
+pcall(lsp.setup)
