0b146bc95aad326ca01824a0cd58b651c481d4a6
Masatoshi N <nishiguchi.masa@gmail.com>
2023-05-24 17:12:59 -0400

    git horizontal split

--- neovim/after/plugin/fugitive.lua
+++ neovim/after/plugin/fugitive.lua
@@ -1,4 +1,4 @@
-vim.keymap.set('n', '<leader>gs', ':vertical Git<CR>', { desc = ':Git' })
+vim.keymap.set('n', '<leader>gs', vim.cmd.Git, { desc = ':Git' })
 vim.keymap.set('n', '<leader>gd', vim.cmd.Gdiff, { desc = ':Gdiff' })
 vim.keymap.set('n', '<leader>gw', vim.cmd.Gwrite, { desc = ':Gwrite' })
 vim.keymap.set('n', '<leader>gr', vim.cmd.Gread, { desc = ':Gread' })
