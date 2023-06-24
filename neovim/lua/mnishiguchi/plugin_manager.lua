f11e3e9c93a5140224ba1eaa1a4563080a2825cb
Masatoshi N <nishiguchi.masa@gmail.com>
2023-06-23 22:50:29 -0400

    fix(nvim): external snippets

--- neovim/lua/mnishiguchi/plugin_manager.lua
+++ neovim/lua/mnishiguchi/plugin_manager.lua
@@ -149,23 +149,44 @@ require('lazy').setup({
     'VonHeikemen/lsp-zero.nvim',
     branch = 'v2.x',
     config = function()
-      local lsp = require('lsp-zero').preset({
-        -- https://github.com/VonHeikemen/lsp-zero.nvim/blob/v2.x/doc/md/autocomplete.md#recommended-sources
-        manage_nvim_cmp = {
-          set_sources = 'recommended'
-        }
-      })
+      local function setup_lsp()
+        local lsp = require('lsp-zero').preset({ })
 
-      -- https://github.com/williamboman/mason-lspconfig.nvim
-      lsp.ensure_installed({'elixirls', 'lua_ls', 'solargraph'})
+        -- https://github.com/williamboman/mason-lspconfig.nvim
+        lsp.ensure_installed({'elixirls', 'lua_ls', 'solargraph'})
 
-      lsp.setup()
+        lsp.setup()
+      end
+
+      -- https://github.com/VonHeikemen/lsp-zero.nvim/blob/v2.x/doc/md/autocomplete.md#add-an-external-collection-of-snippets
+      local function setup_autocompletion()
+        local cmp = require('cmp')
+        local cmp_action = require('lsp-zero').cmp_action()
+
+        require('luasnip.loaders.from_vscode').lazy_load()
+
+        cmp.setup({
+          sources = {
+            {name = 'nvim_lsp'},
+            {name = 'path'},
+            {name = 'luasnip', keyword_length = 2},
+            {name = 'buffer', keyword_length = 5},
+          },
+          mapping = {
+            ['<C-f>'] = cmp_action.luasnip_jump_forward(),
+            ['<C-b>'] = cmp_action.luasnip_jump_backward(),
+          }
+        })
+      end
+
+      setup_lsp()
+      setup_autocompletion()
     end,
     dependencies = {
       -- LSP Support
       {'neovim/nvim-lspconfig'},             -- Required
-      {'williamboman/mason-lspconfig.nvim'}, -- Optional
-      {                                      -- Optional
+      {'williamboman/mason-lspconfig.nvim'},
+      {
         'williamboman/mason.nvim',
         build = function()
           pcall(vim.cmd, 'MasonUpdate')
@@ -178,17 +199,20 @@ require('lazy').setup({
       'hrsh7th/cmp-buffer',
       'hrsh7th/cmp-path',
       'saadparwaiz1/cmp_luasnip',
-      {                       -- Required
-        "L3MON4D3/LuaSnip",
-        dependencies = {
-          {
-            -- https://github.com/rafamadriz/friendly-snippets
-            'rafamadriz/friendly-snippets',
-            init = function()
-              require("luasnip.loaders.from_vscode").lazy_load()
-            end,
-          },
-        },
+      "L3MON4D3/LuaSnip",     -- Required
+    },
+  },
+
+  -- Snippet engine
+  {
+    "L3MON4D3/LuaSnip",
+    dependencies = {
+      {
+        -- https://github.com/rafamadriz/friendly-snippets
+        'rafamadriz/friendly-snippets',
+        init = function()
+          require("luasnip.loaders.from_vscode").lazy_load()
+        end,
       },
     },
   },
