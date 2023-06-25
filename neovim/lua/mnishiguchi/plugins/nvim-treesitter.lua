de18aaeb341dac31a903acd0a7f48896489bd642
Masatoshi N <nishiguchi.masa@gmail.com>
2023-06-25 19:58:37 -0400

    fix(nvim): nvim-treesitter :TSUpdate

--- neovim/lua/mnishiguchi/plugins/nvim-treesitter.lua
+++ neovim/lua/mnishiguchi/plugins/nvim-treesitter.lua
@@ -1,9 +1,8 @@
 -- Highlight, edit, and navigate code
 return {
   "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
   config = function()
-    pcall(require("nvim-treesitter.install").update({ with_sync = true }))
-
     require("nvim-treesitter.configs").setup({
       -- A list of parsers that should always be installed
       ensure_installed = { "c", "elixir", "help", "javascript", "lua", "ruby", "vim" },
