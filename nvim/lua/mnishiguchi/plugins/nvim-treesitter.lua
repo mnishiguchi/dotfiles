c4f9c1f7cb6c163507360977839f77b95a24034c
Masatoshi Nishiguchi <nishiguchi.masa@gmail.com>
2023-10-15 16:59:12 -0400

    chore(nvim): update treesitter settings

--- nvim/lua/mnishiguchi/plugins/nvim-treesitter.lua
+++ nvim/lua/mnishiguchi/plugins/nvim-treesitter.lua
@@ -5,11 +5,13 @@ return {
   config = function()
     require("nvim-treesitter.configs").setup({
       -- A list of parsers that should always be installed
-      ensure_installed = { "c", "elixir", "help", "javascript", "lua", "ruby", "vim" },
+      ensure_installed = { "c", "elixir", "javascript", "lua", "ruby", "vim", "vimdoc" },
       -- Install parsers synchronously (only applied to `ensure_installed`)
       sync_install = false,
       -- Automatically install missing parsers when entering buffer
       auto_install = true,
+      -- List of parsers to ignore installing (or "all")
+      ignore_install = { "help" },
       highlight = {
         enable = true,
         additional_vim_regex_highlighting = false,
