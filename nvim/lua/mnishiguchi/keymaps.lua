6eceb2ec03bc8987864cd412e02c623934c906cb
Masatoshi Nishiguchi <7563926+mnishiguchi@users.noreply.github.com>
2025-01-18 19:48:25 +0900

    nvim: improve keymaps

--- nvim/lua/mnishiguchi/keymaps.lua
+++ nvim/lua/mnishiguchi/keymaps.lua
@@ -5,66 +5,79 @@ local command = vim.api.nvim_create_user_command
 
 -- Set <space> as the leader key
 --  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
-keymap({ "n", "v" }, "<Space>", "<Nop>", { desc = "no op" })
+keymap({ "n", "v" }, "<Space>", "<Nop>", { desc = "Unbind space" })
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
--- show the file explore
-keymap("n", "<leader>e", vim.cmd.Explore, { desc = ":Explore" })
+-- File Explorer
+keymap("n", "<leader>e", ":Ex<CR>", { desc = "Open file explorer" })
 
--- move the highlighted lines around in visual mode
-keymap("v", "J", ":move '>+1<CR>gv=gv", { desc = "move highlighted lines down" })
-keymap("v", "K", ":move '<-2<CR>gv=gv", { desc = "move highlighted lines up" })
-keymap("v", "H", "<gv", { desc = "move highlighted lines left" })
-keymap("v", "L", ">gv", { desc = "move highlighted lines right" })
+-- Visual Mode Line Movements
+keymap("v", "J", ":move '>+1<CR>gv=gv", { desc = "Move lines down" })
+keymap("v", "K", ":move '<-2<CR>gv=gv", { desc = "Move lines up" })
+keymap("v", "H", "<gv", { desc = "Indent left (repeatable)" })
+keymap("v", "L", ">gv", { desc = "Indent right (repeatable)" })
+keymap("v", "<", "<gv", { desc = "Indent left (repeatable)" })
+keymap("v", ">", ">gv", { desc = "Indent right (repeatable)" })
 
--- Stay in indent mode
-keymap("v", "<", "<gv", { desc = "move highlighted lines left" })
-keymap("v", ">", ">gv", { desc = "move highlighted lines right" })
+-- Cursor Management
+keymap("n", "J", "mzJ`z", { desc = "Join lines and keep cursor position" })
+keymap("n", "j", "jzz", { desc = "Move down (centered)" })
+keymap("n", "k", "kzz", { desc = "Move up (centered)" })
+keymap("n", "<Up>", "<Up>zz", { desc = "Move up (centered)" })
+keymap("n", "<Down>", "<Down>zz", { desc = "Move down (centered)" })
+keymap("n", "<C-u>", "<C-u>zz", { desc = "Scroll up (centered)" })
+keymap("n", "<C-d>", "<C-d>zz", { desc = "Scroll down (centered)" })
 
--- keep the cursor in the same place while joining lines
-keymap("n", "J", "mzJ`z")
+-- Search Enhancements
+keymap("n", "n", "nzzzv", { desc = "Next search result (centered)" })
+keymap("n", "N", "Nzzzv", { desc = "Previous search result (centered)" })
 
--- keep the cursor centered while moving up and down
-keymap("n", "j", "jzz")
-keymap("n", "k", "kzz")
-keymap("n", "<C-d>", "<C-d>zz")
-keymap("n", "<C-u>", "<C-u>zz")
-keymap("n", "<Up>", "<Up>zz")
-keymap("n", "<Down>", "<Down>zz")
+-- Disable Accidental Commands
+keymap({ "n", "v" }, "<leader>p", "<Nop>", { desc = "Disable accidental pasting with leader" })
 
--- keep the cursor centered while searching
-keymap("n", "n", "nzzzv")
-keymap("n", "N", "Nzzzv")
+-- Yank and Paste Improvements
+keymap("v", "p", '"_dP', { desc = "Paste over selection without yanking" })
+keymap("n", "<leader>d", '"_d', { desc = "Delete without yanking" })
+keymap("v", "<leader>d", '"_d', { desc = "Delete without yanking" })
+keymap("n", "x", '"_x', { desc = "Delete character without yanking" })
 
--- disable <leader>p to avoid unexpected pasting
-keymap({ "n", "v" }, "<leader>p", "<Nop>", { desc = "no op" })
-
--- put the yanked text onto the visually selected text
-keymap("v", "p", '"_dP', { desc = "[p]aste the yanked onto the visually selected" })
-
--- delete without overwriting the unnamed register
-keymap("n", "<leader>d", '"_d', { desc = "[d]elete without writing unnamed register" })
-keymap("v", "<leader>d", '"_d', { desc = "[d]elete without writing unnamed register" })
-keymap("n", "x", '"_x', { desc = "delete without writing unnamed register" })
-
--- find and replace what the cursor is currently is on
+-- Find and Replace
 keymap("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/cgI<Left><Left><Left><Left>]], {
-	desc = "[s]ubstitute what cursor is currently is on",
+  desc = "Substitute word under cursor",
 })
 
--- make tags for tag-jumping
-keymap("n", "<leader>ct", ":!ctags -R .<CR>", { desc = "make [ct]ags" })
-keymap("n", "tt", "<C-]>")
+-- Tag Management
+keymap("n", "<leader>ct", function()
+  local output = vim.fn.system("ctags -R .")
+  if vim.v.shell_error == 0 then
+    vim.notify("Tags generated", vim.log.levels.INFO)
+  else
+    vim.notify("Error generating tags: " .. output, vim.log.levels.ERROR)
+  end
+end, { desc = "Generate tags (ctags)" })
+keymap("n", "tt", "<C-]>", { desc = "Jump to tag definition" })
 
--- close buffers
-keymap("n", "<leader>q", ":<C-u>bdelete!<CR>", { desc = "[q]uit current buffer" })
+-- Buffer Management
+keymap("n", "<leader>q", ":<C-u>bdelete!<CR>", { desc = "Close current buffer" })
 
--- Copy the link to the line of a Git repository to the clipboard
-keymap("n", "<leader>v", ":.GBrowse!<CR>")
-keymap("x", "<leader>v", ":GBrowse!<CR>")
+-- Git Integration
+keymap("n", "<leader>v", ":.GBrowse!<CR>", { desc = "Copy Git link to current line" })
+keymap("x", "<leader>v", ":GBrowse!<CR>", { desc = "Copy Git link for selection" })
 
--- bind :Q to :q
+-- Increment and Decrement Numbers
+keymap("n", "<leader>=", "<C-a>", { desc = "Increment number" })
+keymap("n", "<leader>-", "<C-x>", { desc = "Decrement number" })
+
+-- Exit Insert Mode Quickly
+keymap("i", "jk", "<ESC>", { desc = "Exit insert mode" })
+keymap("i", "kj", "<ESC>", { desc = "Exit insert mode" })
+
+-- Window Splitting
+keymap("n", "<C-w>\\", ":vsplit<CR>", { desc = "Vertical split" })
+keymap("n", "<C-w>-", ":split<CR>", { desc = "Horizontal split" })
+
+-- Command Aliases
 command("Q", "q", {})
 command("W", "w", {})
 command("WQ", "wq", {})
@@ -72,10 +85,5 @@ command("Wq", "wq", {})
 command("QA", "qa", {})
 command("Qa", "qa", {})
 
--- increment/decrement numbers
-keymap("n", "<leader>=", "<C-a>") -- increment
-keymap("n", "<leader>-", "<C-x>") -- decrement
-
--- Press jk fast to exit insert mode
-keymap("i", "jk", "<ESC>")
-keymap("i", "kj", "<ESC>")
+-- Save and Source
+keymap("n", "<leader><CR>", ":w<CR>:source %<CR>", { desc = "Save and source this file" })
