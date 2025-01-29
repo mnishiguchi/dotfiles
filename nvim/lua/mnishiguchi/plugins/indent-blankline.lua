be9bb2d73427becaac9d1923aa374e0fdd4cd594
Masatoshi Nishiguchi <7563926+mnishiguchi@users.noreply.github.com>
2025-01-30 07:43:05 +0900

    nvim: highlight indent blank lines

--- nvim/lua/mnishiguchi/plugins/indent-blankline.lua
+++ nvim/lua/mnishiguchi/plugins/indent-blankline.lua
@@ -3,7 +3,25 @@ return {
   "lukas-reineke/indent-blankline.nvim",
   main = "ibl",
   config = function()
-    require("ibl").setup()
+    local highlight = {
+      "RainbowBrown",
+      "RainbowBeige",
+      "RainbowCoolGray",
+      "RainbowSlateGray",
+      "RainbowDarkSlate",
+      "RainbowCharcoal",
+    }
+
+    local hooks = require "ibl.hooks"
+    hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+      vim.api.nvim_set_hl(0, "RainbowBrown", { fg = "#8D6E63" })
+      vim.api.nvim_set_hl(0, "RainbowBeige", { fg = "#A1887F" })
+      vim.api.nvim_set_hl(0, "RainbowCoolGray", { fg = "#B0BEC5" })
+      vim.api.nvim_set_hl(0, "RainbowSlateGray", { fg = "#78909C" })
+      vim.api.nvim_set_hl(0, "RainbowDarkSlate", { fg = "#546E7A" })
+      vim.api.nvim_set_hl(0, "RainbowCharcoal", { fg = "#37474F" })
+    end)
+
+    require("ibl").setup { indent = { highlight = highlight } }
   end,
-  opts = {},
 }
