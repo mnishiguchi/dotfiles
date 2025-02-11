3d98010058ab67f54048ffd57b4a9fdff6d8f85a
Masatoshi Nishiguchi <7563926+mnishiguchi@users.noreply.github.com>
2025-02-11 14:49:48 +0900

    nvim: improve autocmds

--- nvim/lua/mnishiguchi/autocmds.lua
+++ nvim/lua/mnishiguchi/autocmds.lua
@@ -6,39 +6,26 @@
 
 local mnishiguchi_augroup = vim.api.nvim_create_augroup("mnishiguchi_augroup", {})
 
--- Strip trailing whitespace on write
-vim.api.nvim_create_autocmd("BufWritePre", {
-  group = mnishiguchi_augroup,
-  pattern = "*",
-  command = [[%s/\s\+$//e]],
-})
-
--- Highlight on yank
-vim.api.nvim_create_autocmd("TextYankPost", {
-  group = mnishiguchi_augroup,
-  pattern = "*",
-  callback = vim.highlight.on_yank,
-})
-
--- Open quick fix window on grep
-vim.api.nvim_create_autocmd("QuickFixCmdPost", {
-  group = mnishiguchi_augroup,
-  pattern = "*grep*",
-  command = "cwindow",
-})
-
 -- Dynamically detect filetypes based on filename patterns
 local filetype_patterns = {
-  ["*.rasi"] = "js",
   ["*.livemd"] = "markdown",
-  ["Gemfile,Rakefile,Vagrantfile,Thorfile,Guardfile,config.ru,*.rake,*.jbuilder"] = "ruby",
+  ["Gemfile"] = "ruby",
+  ["Rakefile"] = "ruby",
+  ["Vagrantfile"] = "ruby",
+  ["Thorfile"] = "ruby",
+  ["Guardfile"] = "ruby",
+  ["config.ru"] = "ruby",
+  ["*.rake"] = "ruby",
+  ["*.jbuilder"] = "ruby",
 }
 
 for pattern, filetype in pairs(filetype_patterns) do
   vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
     group = mnishiguchi_augroup,
     pattern = pattern,
-    command = "set filetype=" .. filetype,
+    callback = function()
+      vim.opt_local.filetype = filetype
+    end,
   })
 end
 
@@ -52,7 +39,6 @@ vim.api.nvim_create_autocmd("BufReadPre", {
       vim.opt.syntax = "off"        -- Disable syntax highlighting
       vim.opt.swapfile = false      -- Disable swapfile
       vim.opt.undofile = false      -- Disable undo history
-      vim.opt.bufhidden = "unload"  -- Unload the buffer when hidden
       vim.opt.foldmethod = "manual" -- Disable folding to avoid parsing
       vim.notify("Large file detected. Performance settings applied.", vim.log.levels.WARN)
     end
