f7dde173308398ae1821c94b5166f1b8ca797541
Masatoshi Nishiguchi <7563926+mnishiguchi@users.noreply.github.com>
2026-04-01 16:17:07 +0900

    refactor(nvim): simplify telescope keymap option handling

--- nvim/lua/mnishiguchi/plugins/telescope.lua
+++ nvim/lua/mnishiguchi/plugins/telescope.lua
@@ -24,32 +24,36 @@ return {
       local themes = require("telescope.themes")
       local opts = { noremap = true, silent = true }
 
-      vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "List open buffers", unpack(opts) })
-      vim.keymap.set("n", "<leader>fc", builtin.commands, { desc = "Search available Vim commands", unpack(opts) })
-      vim.keymap.set("n", "<leader>fd", builtin.diagnostics, { desc = "Search LSP diagnostics", unpack(opts) })
-      vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Find files in the workspace", unpack(opts) })
+      local function keymap_opts(desc)
+        return vim.tbl_extend("force", opts, { desc = desc })
+      end
+
+      vim.keymap.set("n", "<leader>fb", builtin.buffers, keymap_opts("List open buffers"))
+      vim.keymap.set("n", "<leader>fc", builtin.commands, keymap_opts("Search available Vim commands"))
+      vim.keymap.set("n", "<leader>fd", builtin.diagnostics, keymap_opts("Search LSP diagnostics"))
+      vim.keymap.set("n", "<leader>ff", builtin.find_files, keymap_opts("Find files in the workspace"))
       vim.keymap.set("n", "<leader>fg", function()
         builtin.grep_string({ search = vim.fn.input("Grep> ") })
-      end, { desc = "Search for text in the workspace", unpack(opts) })
-      vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Search help documentation", unpack(opts) })
-      vim.keymap.set("n", "<leader>fk", builtin.keymaps, { desc = "List all key mappings", unpack(opts) })
-      vim.keymap.set("n", "<leader>fl", builtin.loclist, { desc = "Search the location list", unpack(opts) })
-      vim.keymap.set("n", "<leader>fm", builtin.marks, { desc = "Find marks in the workspace", unpack(opts) })
-      vim.keymap.set("n", "<leader>fo", builtin.vim_options, { desc = "Search and tweak Vim options", unpack(opts) })
-      vim.keymap.set("n", "<leader>fq", builtin.quickfix, { desc = "Search the quickfix list", unpack(opts) })
-      vim.keymap.set("n", "<leader>fr", builtin.registers, { desc = "List Vim registers", unpack(opts) })
-      vim.keymap.set("n", "<leader>gb", builtin.git_branches, { desc = "List Git branches", unpack(opts) })
-      vim.keymap.set("n", "<leader>gc", builtin.git_commits, { desc = "View Git commit history", unpack(opts) })
-      vim.keymap.set("n", "<leader>gd", builtin.git_status, { desc = "View Git changes and diffs", unpack(opts) })
-      vim.keymap.set("n", "<leader>gf", builtin.git_files, { desc = "Find files in the Git repository", unpack(opts) })
-      vim.keymap.set("n", "<leader><leader>", builtin.oldfiles, { desc = "Reopen recently used files", unpack(opts) })
-      vim.keymap.set("n", "<leader>?", builtin.live_grep, { desc = "Live grep across the workspace", unpack(opts) })
+      end, keymap_opts("Search for text in the workspace"))
+      vim.keymap.set("n", "<leader>fh", builtin.help_tags, keymap_opts("Search help documentation"))
+      vim.keymap.set("n", "<leader>fk", builtin.keymaps, keymap_opts("List all key mappings"))
+      vim.keymap.set("n", "<leader>fl", builtin.loclist, keymap_opts("Search the location list"))
+      vim.keymap.set("n", "<leader>fm", builtin.marks, keymap_opts("Find marks in the workspace"))
+      vim.keymap.set("n", "<leader>fo", builtin.vim_options, keymap_opts("Search and tweak Vim options"))
+      vim.keymap.set("n", "<leader>fq", builtin.quickfix, keymap_opts("Search the quickfix list"))
+      vim.keymap.set("n", "<leader>fr", builtin.registers, keymap_opts("List Vim registers"))
+      vim.keymap.set("n", "<leader>gb", builtin.git_branches, keymap_opts("List Git branches"))
+      vim.keymap.set("n", "<leader>gc", builtin.git_commits, keymap_opts("View Git commit history"))
+      vim.keymap.set("n", "<leader>gd", builtin.git_status, keymap_opts("View Git changes and diffs"))
+      vim.keymap.set("n", "<leader>gf", builtin.git_files, keymap_opts("Find files in the Git repository"))
+      vim.keymap.set("n", "<leader><leader>", builtin.oldfiles, keymap_opts("Reopen recently used files"))
+      vim.keymap.set("n", "<leader>?", builtin.live_grep, keymap_opts("Live grep across the workspace"))
       vim.keymap.set("n", "<leader>/", function()
         builtin.current_buffer_fuzzy_find(themes.get_dropdown({
           winblend = 10,
           previewer = false,
         }))
-      end, { desc = "Fuzzy search in the current buffer", unpack(opts) })
+      end, keymap_opts("Fuzzy search in the current buffer"))
     end,
     dependencies = {
       "nvim-lua/plenary.nvim", -- Required dependency for Telescope
