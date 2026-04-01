e4848e3dd42718df4b6140bf4536531df8e68fd3
Masatoshi Nishiguchi <7563926+mnishiguchi@users.noreply.github.com>
2026-04-01 16:12:41 +0900

    refactor(nvim): make autocmds reload-safe and large-file settings local

--- nvim/lua/mnishiguchi/autocmds.lua
+++ nvim/lua/mnishiguchi/autocmds.lua
@@ -4,7 +4,9 @@
 -- see https://neovim.io/doc/user/autocmd.html
 -------------------------------------------------------------------------------
 
-local mnishiguchi_augroup = vim.api.nvim_create_augroup("mnishiguchi_augroup", {})
+local mnishiguchi_augroup = vim.api.nvim_create_augroup("mnishiguchi_augroup", {
+  clear = true,
+})
 
 -- Dynamically detect filetypes based on filename patterns
 local filetype_patterns = {
@@ -23,8 +25,8 @@ for pattern, filetype in pairs(filetype_patterns) do
   vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
     group = mnishiguchi_augroup,
     pattern = pattern,
-    callback = function()
-      vim.opt_local.filetype = filetype
+    callback = function(args)
+      vim.bo[args.buf].filetype = filetype
     end,
   })
 end
@@ -33,14 +35,27 @@ end
 vim.api.nvim_create_autocmd("BufReadPre", {
   group = mnishiguchi_augroup,
   pattern = "*",
-  callback = function()
-    local max_size = 1024 * 1024    -- 1 MB
-    if vim.fn.getfsize(vim.fn.expand("%")) > max_size then
-      vim.opt.syntax = "off"        -- Disable syntax highlighting
-      vim.opt.swapfile = false      -- Disable swapfile
-      vim.opt.undofile = false      -- Disable undo history
-      vim.opt.foldmethod = "manual" -- Disable folding to avoid parsing
-      vim.notify("Large file detected. Performance settings applied.", vim.log.levels.WARN)
+  callback = function(args)
+    local max_size = 1024 * 1024 -- 1 MB
+    local file_path = vim.api.nvim_buf_get_name(args.buf)
+    local stat = file_path ~= "" and vim.uv.fs_stat(file_path) or nil
+
+    if stat and stat.size > max_size then
+      vim.bo[args.buf].swapfile = false -- Disable swapfile
+      vim.bo[args.buf].undofile = false -- Disable undo history
+      vim.b[args.buf].large_file = true
+    end
+  end,
+})
+
+vim.api.nvim_create_autocmd("BufWinEnter", {
+  group = mnishiguchi_augroup,
+  pattern = "*",
+  callback = function(args)
+    if vim.b[args.buf].large_file then
+      vim.opt_local.syntax = "off"        -- Disable syntax highlighting
+      vim.opt_local.foldmethod = "manual" -- Disable folding to avoid parsing
+      vim.notify_once("Large file detected. Performance settings applied.", vim.log.levels.WARN)
     end
   end,
 })
