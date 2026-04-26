8b0bdf2cd5378966b96decbc58a6151d0cd3adba
Masatoshi Nishiguchi <7563926+mnishiguchi@users.noreply.github.com>
2026-04-26 18:56:55 +0900

    refactor(elixir): polish shared IEx config

--- elixir/iex.exs
+++ elixir/iex.exs
@@ -1,15 +1,15 @@
-# https://qihttps://fly.io/phoenix-files/taking-control-of-map-sort-order-in-elixir/
 # https://fly.io/phoenix-files/taking-control-of-map-sort-order-in-elixir/
+# https://hexdocs.pm/iex/IEx.html#configure/1
+# https://hexdocs.pm/elixir/Inspect.Opts.html
 
 Application.put_env(:elixir, :dbg_callback, {Macro, :dbg, []})
 Application.put_env(:elixir, :ansi_enabled, true)
 
-# print iex.exs currently used
-[:blue_background, :bright, :white, "loading ", __ENV__.file]
+# Show which IEx config was loaded.
+[:blue_background, :bright, :white, " loading ", __ENV__.file, " "]
 |> IO.ANSI.format()
 |> IO.puts()
 
-# https://hexdocs.pm/iex/IEx.html#configure/1
 IEx.configure(
   default_prompt:
     [:light_magenta, "%prefix>"]
@@ -19,19 +19,25 @@ IEx.configure(
     [:light_magenta, "%prefix(%node)>"]
     |> IO.ANSI.format()
     |> IO.chardata_to_string(),
-  # https://hexdocs.pm/elixir/Inspect.Opts.html#t:t/0
   inspect: [
+    # Uncomment when charlists are printed as text, for example ~c"abc".
     # charlists: :as_lists,
-    custom_options: [sort_maps: true],
+
+    # Uncomment when deeply nested values are collapsed as "...".
     # limit: :infinity,
-    pretty: true,
-    # printable_limit: :infinity
+
+    # Uncomment when long strings, binaries, or lists are truncated.
+    # printable_limit: :infinity,
+
+    # Keep map output stable and easier to compare while debugging.
+    custom_options: [sort_maps: true],
     width: 80
   ],
   history_size: 50,
   width: 80
 )
 
+# Import common helpers only when the current project provides them.
 import_if_available(Plug.Conn)
 import_if_available(Phoenix.HTML)
 import_if_available(Ecto.Query)
