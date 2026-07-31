d27a8cb046685092972cd5b9187ccf20bf693499
Masatoshi Nishiguchi <7563926+mnishiguchi@users.noreply.github.com>
2026-07-31 21:40:16 +0900

    refactor(nvim): migrate plugins to vim.pack

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,19 +3,4 @@ require("mnishiguchi.keymaps")
 require("mnishiguchi.filetypes")
 require("mnishiguchi.autocmds")
 
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-
-if not vim.uv.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable",
-		lazypath,
-	})
-end
-
-vim.opt.rtp:prepend(lazypath)
-
-require("lazy").setup(require("mnishiguchi.plugins"))
+require("mnishiguchi.plugins")
